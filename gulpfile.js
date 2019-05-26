0d4049c456753cf8b515d7e8507638084643f0cb
Kaai Suzuki <suzydp@me.com>
2019-05-26 02:38:35 -0700

    Setup gulpfiles and sass compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,35 @@
-function defaultTask(cb) {
-  cb();
+const gulp = require('gulp');
+const { series, watch } = require('gulp');
+const { src, dest } = require('gulp');
+const path = require('path');
+const fs = require('fs');
+const pkg = JSON.parse(fs.readFileSync('./package.json'));
+const assetsPath = path.resolve(pkg.path.assetsDir);
+
+// to compile sass
+const sass = require('gulp-sass');
+
+// add vender prefix
+const autoprefixer = require('gulp-autoprefixer');
+
+// error handling
+const plumber = require('gulp-plumber');
+
+function compileSass() {
+  return gulp.src(path.join(assetsPath, 'scss/main.scss'))
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer(
+      {
+        browsers: ['last 2 versions'],
+        cascade: false
+      }
+    ))
+    .pipe(gulp.dest(path.join(assetsPath, 'css')))
 }
 
-exports.default = defaultTask;
+function watchSass() {
+  return gulp.watch(path.join(assetsPath, 'sass/**/*.scss'), gulp.task('sass'));
+}
+
+exports.default = series(compileSass, watchSass);
